338e497c9477c426a87d8478c65a7c3df8f61159
edwinosayande <edwinosayande@gmail.com>
2022-02-20 16:47:41 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import { CardList } from './components/card-list/card-list-components';
+import {SearchBox} from './components/search-box/search-box-components';
 
-function App() {
+
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      robots: [],
+      searchField: ''
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({robots: users}));
+  }
+  render(){
+    const { robots, searchField } = this.state;
+    const filterRobots = robots.filter(robot =>
+      robot.name.toLowerCase().includes(searchField.toLowerCase()));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Robots Rolodex</h1>
+       <SearchBox
+        placeholder='Search Robots'
+        handleChange={e => this.setState({ searchField: e.target.value })}
+       />
+      <CardList robots={filterRobots} />
     </div>
   );
+}
 }
 
 export default App;
